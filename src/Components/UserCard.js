487a08ab707e6b7cc407fda49009f21471f995ff
pandabearcoding <madelinerose16@outlook.com>
2021-01-20 21:35:19 -0500

    week 2, commit 3

--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -1,10 +1,73 @@
 import React from 'react';
 
 class UserCard extends React.Component {
+
+    state = {
+        clicked: false,
+        username: this.props.user.username,
+        password: this.props.user.password, 
+        name: this.props.user.name, 
+        profile_picture: this.props.user.profile_picture, 
+        email: this.props.user.email,
+        phone_number: this.props.user.phone_number,
+        address: this.props.user.address,
+        cc_number: this.props.user.cc_number,
+        cc_cvv: this.props.user.cc_cvv,
+        cc_date: this.props.user.cc_date,
+    }
+
+    updateClickHandler = (e) => {
+        this.setState({clicked: true})
+    }
+
+    changeHandler = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    localUpdateHandler = (e) => {
+        e.preventDefault()
+        this.props.updateHandler(this.props.user.id, this.state.username, this.state.password, this.state.name, this.state.profile_picture, this.state.email, this.state.phone_number, this.state.address, this.state.cc_number, this.state.cc_cvv, this.state.cc_date)
+        this.setState(previousState => ({
+            clicked: !previousState.clicked
+        }))
+    }
+
+    localDeleteHandler = (e) => {
+        this.props.deleteHandler(this.props.user.id)
+    }
+
     render(){
+        let { username, password, name, profile_picture, email, phone_number, address, cc_number, cc_cvv, cc_date } = this.props.user
         return(
-            <div>
-                User Card
+            <div className="userCard">
+            {this.state.clicked ? (
+                <form onSubmit={this.localUpdateHandler}>
+                    <input name="username" type= "text" value={this.state.username} onChange={this.changeHandler} />
+                    <input name="password" type= "text" value={this.state.password} onChange={this.changeHandler} />
+                    <input name="name" type="text" value={this.state.name} onChange={this.changeHandler} />
+                    <input name="email" type="text" value={this.state.email} onChange={this.changeHandler} />
+                    <input name="profile_picture" type= "text" value={this.state.profile_picture} onChange={this.changeHandler} />
+                    <input name="address" type= "text" value={this.state.address} onChange={this.changeHandler} />
+                    <input name="cc_number" type= "text" value={this.state.cc_number} onChange={this.changeHandler} />
+                    <input name="cc_cvv" type= "text" value={this.state.cc_cvv} onChange={this.changeHandler} />
+                    <input name="cc_date" type= "text" value={this.state.cc_date} onChange={this.changeHandler} />
+                    <button type="submit">Update Profile</button>
+                </form>
+            ) :
+            null
+            }
+                <h1>{username}</h1>
+                <h3>{name}</h3>
+                <img alt="" src={profile_picture}/>
+                <p>Phone Number: <span>{phone_number}</span></p>
+                <p> Email: <span>{email}</span></p>
+                <p>Address: <span>{address}</span></p>
+                <p>Credit Card Number: <span>{cc_number}</span></p>
+                <p>Credit Card Date: <span>{cc_date}</span></p>
+                <p>Credit Card CVV: <span>{cc_cvv}</span></p>
+
+                <button onClick={this.updateClickHandler}>Edit Profile</button>
+                <button onClick={this.localDeleteHandler}>Delete Profile</button>
             </div>
         )
     }
--- src/Containers/UserContainer.js
+++ src/Containers/UserContainer.js
@@ -7,8 +7,71 @@ class UserContainer extends React.Component {
         userApi: []
     }
 
+    componentDidMount(){
+        fetch("http://localhost:4000/users/")
+        .then(response => response.json())
+        .then(user => this.setState({userApi: user}))
+        .catch(console.log)
+    }
+
+    newUserSubmitHandler = (newUser) => {
+        fetch('http://localhost:4000/users', {
+            method: "POST", 
+            headers: {
+                "content-type": "application/json",
+                accepts: "application/json"
+            },
+            body: JSON.stringify(newUser)
+          })
+          .then(response => response.json())
+          .then(user => this.setState({userApi: [ user, ...this.state.userApi ]}))
+          .catch(console.log)
+        }
+
+    updateSubmitHandler = (id, username, password, name, profile_picture, email, phone_number, address, cc_number, cc_cvv, cc_date) => {
+        let updateUser = { 
+            username: username,
+            password: password,
+            name: name, 
+            profile_picture: profile_picture, 
+            email: email,
+            phone_number: phone_number,
+            address: address,
+            cc_number: cc_number,
+            cc_cvv: cc_cvv,
+            cc_date: cc_date,
+        }
+        fetch(`http://localhost:4000/users/${id}`, {
+            method: "PATCH", 
+            headers: {
+                "content-type": "application/json",
+                accepts: "application/json"
+            },
+            body: JSON.stringify(updateUser)
+        })
+        .then(response => response.json())
+        .then(user => {
+            let newArray = [...this.state.userApi]
+            let foundObject = newArray.find(el => el.id === user.id)
+             newArray[newArray.indexOf(foundObject)] = user
+                this.setState({userApi: newArray})
+        })
+        .catch(console.log)
+    }
+
+    deleteHandler = (userId) => {
+        fetch(`http://localhost:4000/users/${userId}`, {
+             method: "DELETE"
+        })
+        .then(response => response.json())
+        .then((response) => {
+            let newArray = this.state.userApi.filter(user => user.id !== userId)
+            this.setState({userApi: newArray})
+        })
+    }
+
     renderUsers = () => {
-        return this.state.userApi.map((el) => <UserCard key={el.id} user={el} />)
+        return this.state.userApi.map((el) => <UserCard key={el.id} user={el} updateHandler={this.updateSubmitHandler} deleteHandler={this.deleteHandler} />)
     }
 
     render(){
